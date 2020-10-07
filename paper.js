d2d8d39afd7a8008c74f78d40e1ba293099cd49c
Davinci2003 <67372840+Davinci2003@users.noreply.github.com>
2020-10-07 12:25:50 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -2,15 +2,12 @@ class Paper{
     constructor(x,y,radius){
         var options={
             isStatic:false,
-            restitution:0.3,
             friction:0.5,
             density:1.2,
         }
         this.radius = radius;
-        //this.x=x;
-        //this.y=y;
         this.image=loadImage("paper.png");
-        this.body=Bodies.circle(x,y,this.radius,options);
+        this.body=Bodies.circle(x,y,this.radius/2,options);
         World.add(world,this.body);
     }
 
@@ -18,9 +15,10 @@ class Paper{
         var pos = this.body.position;
         push();
         translate(pos.x,pos.y);
-        fill(125);
+        rectMode(CENTER);
+        fill(225,0,225);
         imageMode(CENTER);
-        image(this.image,200,475,this.radius);
+        image(this.image,0,0,this.radius, this.radius);
         pop();
     }
 }
